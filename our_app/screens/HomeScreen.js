571bc172bf757720503de06b0690efe31ec81549
Brogle_Mac <deanogle270@gmail.com>
2019-11-03 01:08:09 -0400

    Added button to app.js

--- our_app/screens/HomeScreen.js
+++ our_app/screens/HomeScreen.js
@@ -7,24 +7,24 @@ import Colors from '../constants/Colors.js';
 import ServiceButton from '../components/ServiceButton.js';
 
 const HomeScreen = props => {
-    return (
-        <View style = {styles.container}>
-            <ServiceButton value = "Add Fit" color = {Colors.primary} function = {props.pickClothing}/>
-            <ServiceButton value = "My Wardrobe" color = {Colors.secondary}/>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <ServiceButton value="Add Fit" color={Colors.primary} function={props.pickClothing} />
+      <ServiceButton value="My Wardrobe" color={Colors.secondary} function={props.openWardrobe} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-    screen: {
-      flex: 1
-    },
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  screen: {
+    flex: 1
+  },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
 
-  export default HomeScreen;
+export default HomeScreen;
---
+++ our_app/screens/RecommendationScreen.js
@@ -0,0 +1,106 @@
+import React, { useState } from 'react';
+import {
+    StyleSheet,
+    Text,
+    View,
+    Button,
+    ActivityIndicator,
+    Alert,
+} from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
+
+import Colors from '../constants/Colors.js';
+
+const RecommendationScreen = props => {
+    const [isFetching, setIsFetching] = useState(false);
+    const [tempInfo, setTempInfo] = useState();
+
+    const averageTemp = () => {
+        if (!tempInfo) {
+            console.log("ERROR CODE 1");
+            return;
+        } else {
+            let avg_temp = 0;
+            let num_temps = 0;
+            tempInfo.list.forEach(
+                (item) => {
+                    if (item.main.temp) {
+                        console.log(item.main.temp);
+                        avg_temp += item.main.temp;
+                        num_temps++;
+                    }
+                }
+            )
+            console.log(avg_temp / num_temps);
+            const fahrenheit = Math.round(((avg_temp / num_temps) - 273.15) * 9 / 5 + 32)
+            console.log(`Fahrenheit: ${fahrenheit}`)
+        }
+    }
+    const verifyPermissions = async () => {
+        const result = await Permissions.askAsync(Permissions.LOCATION);
+        if (result.status !== 'granted') {
+            Alert.alert(
+                'Insufficient permissions!',
+                'You need to grant location permissions to use this app.',
+                [{ text: 'Okay' }]
+            );
+            return false;
+        }
+        return true;
+    };
+
+    const getLocationHandler = async () => {
+        const hasPermission = await verifyPermissions();
+        if (!hasPermission) {
+            return;
+        }
+        try {
+            setIsFetching(true);
+            const location = await Location.getCurrentPositionAsync({ timeout: 5000 });
+        } catch (err) {
+            Alert.alert(
+                'Could Not Fetch Location',
+                'Make sure you give this app permissions to access your location',
+                [{ text: 'Okay' }]
+            );
+        }
+        fetch('http://api.openweathermap.org/data/2.5/forecast?id=4509177&APPID=3e9aa70a180dcbcb04df1224f84ee61b')
+            .then(
+                (response) => {
+                    return response.json();
+                }
+            ).then(
+                (response) => {
+                    //console.log(response);
+                    setTempInfo(response);
+                }
+            )
+        setIsFetching(false);
+    };
+
+    return (
+        <View style={styles.container}>
+            <View>
+                {isFetching ? <ActivityIndicator size='large' color={Colors.primary} /> : <Text>No Location Picked Yet</Text>}
+            </View>
+            <Button title="Get Location" color={Colors.primary} onPress={getLocationHandler} />
+            <Button title="Show Stuff" color={Colors.primary} onPress={averageTemp} />
+        </View>
+
+    );
+}
+
+const styles = StyleSheet.create({
+    screen: {
+        flex: 1
+    },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
+
+export default RecommendationScreen;
